7238c83d455572425efbb821b7e6c80aec63e1e2
kosssm <kossdd@gmail.com>
2017-12-18 13:54:22 +0200

    mod3

---
+++ module - 3/script.js
@@ -0,0 +1,9 @@
+var keyboard = [
+    ["qwertyuiop[]"],
+    ["asdfghjkl;'"],
+    ['zxcvbnm,./";']
+];
+
+alert(keyboard[1][0][5] + keyboard[0][0][2] + keyboard[1][0][8] + keyboard[1][0][8] + keyboard[0][0][8]);
+alert(keyboard[1][0][6] + keyboard[1][0][0] + keyboard[2][0][3] + keyboard[1][0][0] + keyboard[1][0][1] + keyboard[2][0][2] + keyboard[0][0][3] + keyboard[0][0][7] + keyboard[0][0][9] + keyboard[0][0][4]);
+alert(keyboard[0][0][4] + keyboard[0][0][3] + keyboard[1][0][0] + keyboard[0][0][7] + keyboard[2][0][5] + keyboard[0][0][2] + keyboard[0][0][3]);
